cc015c8931a24217bd235b1cccde3ea4302cf9ff
Andreas Kristiansen <andkr@dfds.com>
2026-01-16 16:25:30 +0100

    Implemented basic CLI args support.

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,53 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var paths = try getProjectFilePaths("c:\\GitRepositories\\Services.Pax.OfferValidity\\Services.Pax.OfferValidity.slnx", allocator);
+    var args_iter = try std.process.argsWithAllocator(allocator);
+    defer args_iter.deinit();
+
+    // Skip executable name
+    _ = args_iter.next();
+
+    const cmd = args_iter.next() orelse {
+        try printHelp();
+        return;
+    };
+
+    if (std.mem.eql(u8, cmd, "upgrade")) {
+
+        // "c:\\GitRepositories\\Services.Pax.OfferValidity\\Services.Pax.OfferValidity.slnx"
+
+        try cmdUpgrade(allocator, &args_iter);
+    } else if (std.mem.eql(u8, cmd, "help")) {
+        try cmdHelp();
+    } else {
+        std.debug.print("Unknown command: {s}\n\n", .{cmd});
+        try printHelp();
+        return;
+    }
+}
+
+fn cmdUpgrade(allocator: std.mem.Allocator, args: *std.process.ArgIterator) !void {
+    var filename: ?[]const u8 = null;
+
+    var dry = false;
+    var force = false;
+
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "--dry-run")) {
+            dry = true;
+        } else if (std.mem.eql(u8, arg, "--force")) {
+            force = true;
+        } else if (filename == null) {
+            filename = arg;
+        }
+    }
+
+    if (filename == null) {
+        std.debug.print("upgrade: missing filename\n", .{});
+        return;
+    }
+
+    var paths = try getProjectFilePaths(filename.?, allocator);
     defer {
         for (paths.items) |s| allocator.free(s);
         paths.deinit(allocator);
@@ -15,6 +61,10 @@ pub fn main() !void {
     for (paths.items) |item| {
         std.debug.print("{s}\n", .{item});
     }
+}
+
+fn cmdHelp() !void {
+    try printHelp();
 }
 
 fn getProjectFilePaths(solution_path: []const u8, allocator: std.mem.Allocator) !std.ArrayList([]const u8) {
@@ -49,4 +99,23 @@ fn extractBetweenQuotes(s: []const u8) ?[]const u8 {
     const e_index = std.mem.indexOfScalar(u8, rest, '"') orelse return null;
 
     return rest[0..e_index];
+}
+
+fn printHelp() !void {
+    var buf: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&buf);
+
+    const stdout = &stdout_writer.interface;
+
+    try stdout.print(
+        \\Usage:
+        \\  Znuget <command> [options]
+        \\
+        \\Commands:
+        \\  upgrade <solution file> [--dry-run] [--force]
+        \\  help
+        \\
+    , .{});
+
+    try stdout.flush();
 }
